82770982f61921c2f9a38300d7f847bf0c019fbb
zqj <3483353842@qq.com>
2025-12-18 22:44:53 +0800

    1.8.4：修改自动更新为SCP模式

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ app = FastAPI()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
-APP_VERSION = "v1.8.3"
+APP_VERSION = "v1.8.4"
 
 templates.env.globals["app_version"] = APP_VERSION
 
