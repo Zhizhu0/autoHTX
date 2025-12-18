c14042d5854439980f72c5038108204c82c2c6c0
zqj <3483353842@qq.com>
2025-12-18 22:37:28 +0800

    1.8.3：增加版本号显示

--- main.py
+++ main.py
@@ -65,6 +65,10 @@ app = FastAPI()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
+APP_VERSION = "v1.8.3"
+
+templates.env.globals["app_version"] = APP_VERSION
+
 chart_gen = ChartGenerator()
 
 
